3c1028a97c94e597f15adcd488ad73477b80804f
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-10-07 01:41:24 +0000

    updating index
    [x] colors updated
    [x] few tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,18 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react({
+      include: ["***/react/*"],
+    }),
+  ],
   output: "server",
 
   adapter: node({
